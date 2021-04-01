7da51b9e5d137bc28f7813bb0ab84d417478c025
DarwinYouTube <81471376+DarwinYouTube@users.noreply.github.com>
2021-04-01 12:27:41 +0800

    Add files via upload

---
+++ Beep/Beep.cpp
@@ -0,0 +1,38 @@
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+
+
+    Beep(247, 500);
+    Beep(417, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(417, 500);
+    Beep(329, 500);
+    Beep(247, 500);
+    Beep(247, 500);
+    Beep(247, 500);
+    Beep(417, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(417, 500);
+    Beep(497, 500);
+    Sleep(500);
+    Beep(497, 500);
+    Beep(277, 500);
+    Beep(277, 500);
+    Beep(440, 500);
+    Beep(440, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(329, 500);
+    Beep(247, 500);
+    Beep(417, 500);
+    Beep(417, 500);
+    Beep(370, 500);
+    Beep(417, 500);
+    Beep(329, 500);
+
+
+}
